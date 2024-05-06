45e44ece1da569d135d701ab9a45f38d1015ad4b
kim <rook@Rooks-Laptop.local>
2024-05-06 02:58:47 -0600

    add animation to project iamges

--- app/mainPage/projects.tsx
+++ app/mainPage/projects.tsx
@@ -25,8 +25,11 @@ type ProjectProps = (typeof projectsData)[number];
 
 function Project({ title, description, imageUrl, tags }: ProjectProps) {
   return (
-    <section className="group bg-gray-100 sm:h-[20rem] border border-black/5 overflow-hidden 
-    sm:pr-8 even:pr-0 relative sm:flex content-center sm:even:flex-row-reverse mb-5 last:mb-0 ">
+    <section
+      className="group bg-gray-100 sm:h-[25rem] border border-black/5 overflow-hidden 
+        sm:pr-8 even:pr-0 relative sm:flex content-center sm:even:flex-row-reverse mb-5 last:mb-0
+        hover:bg-gray-200 hover:cursor-pointer  transition-all"
+    >
       <div className="pt-5 pb-8 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full">
         <h3 className="text-2xl">{title}</h3>
         <p className="mt-2 leading-relaxed text-gray-700">{description}</p>
@@ -45,8 +48,17 @@ function Project({ title, description, imageUrl, tags }: ProjectProps) {
         src={imageUrl}
         alt={title}
         quality={95}
-        className="absolute top-8 -right-40 w-[32rem] rounded-t-lg shadow-2xl 
-        group-even:right-[initial] group-even:-left-40"
+        className="absolute top-8 -right-40 w-[32rem] rounded-t-lg shadow-2xl group-even:right-[initial] group-even:-left-40
+        transition
+        group-hover:scale-[1.05]
+        group-hover:-translate-x-3
+        group-hover:translate-y-3
+        group-hover:-rotate-2
+
+        group-even:group-hover:translate-y-3
+        group-even:group-hover:rotate-2
+        group-even:group-hover:translate-x-3
+"
       />
     </section>
   );
