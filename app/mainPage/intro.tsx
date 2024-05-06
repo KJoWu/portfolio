669c4a7a9cdb95ae2590d018c94d62e56e86abf2
kim <rook@Rooks-Laptop.local>
2024-05-06 00:07:08 -0600

    add animation to button

--- app/mainPage/intro.tsx
+++ app/mainPage/intro.tsx
@@ -23,38 +23,35 @@ export default function intro() {
         >
           <p className="text-5xl">Hi, I'm Kim</p>
           <p className="text-1xl pt-5 ">
-            Front end developer, product designer and artist with 8 years of
+            I'm a <b>front end developer</b>, <b>product designer</b> and <b>artist</b> with <b>8 years</b> of
             experience
           </p>
           <p className="">
             Currently working on next gen products involving llm and AI
           </p>
-          
         </motion.div>
 
         <motion.div
           className="flex flex-col sm:flex-row text-sm mt-9 gap-2"
           initial={{ opacity: 0, y: 100 }}
-          animate={{ opacity: 1, y: 0}}
+          animate={{ opacity: 1, y: 0 }}
           transition={{
             delay: 0.9,
           }}
         >
-          <button className="bg-gray-900 hover:bg-gray-800 text-white px-4 py-2 rounded-full">
+          <button className="bg-gray-900 hover:scale-110 transition hover:bg-gray-800 text-white px-4 py-2 rounded-full">
             Contact Me
           </button>
-          <button className="bg-white hover:bg-gray-100 px-4 rounded-full  text-gray-700">
+          <button className="bg-white hover:bg-gray-100 px-4 rounded-full hover:scale-110 transition text-gray-700">
             My CSV
           </button>
-          <button className="bg-white hover:bg-gray-100 width-auto p-3 rounded-full text-gray-700">
+          <button className="bg-white hover:bg-gray-100 width-auto p-3 hover:scale-110 transition rounded-full text-gray-700">
             <BsLinkedin />
           </button>
         </motion.div>
       </div>
 
-      <div> 
-        Icon on Right Use a big picture 
-        </div>
+      <div>Icon on Right Use a big picture</div>
     </section>
   );
 }
