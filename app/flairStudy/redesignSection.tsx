e051381a28cf26501f732f9dacf485d440a395d7
kim <rook@Rooks-Laptop.local>
2024-06-20 17:12:24 -0600

    update fonts for redesign

--- app/flairStudy/redesignSection.tsx
+++ app/flairStudy/redesignSection.tsx
@@ -16,14 +16,14 @@ export const RedesignSection = () => {
       <div className="space-y-4">
         <div>
           <h2 className="text-xl font-semibold text-gray-800">Benchmarking Competitor Successes to Enhance UI Design</h2>
-          <p className="mb-6 text-gray-700 leading-8 mt-6">
+          <p className="mb-6 text-gray-700leading-7mt-6">
             We will examine top Canadian competitors like Air Canada, as well as other airlines that have successfully implemented boarding passes, such as Delta and American Airlines, to improve the UI.
           </p>
         </div>
 
         <div>
           <h2 className="text-xl font-semibold text-gray-800">Consistency in Theme and Colors</h2>
-          <p className="mb-6 text-gray-700 leading-8 mt-6">
+          <p className="mb-6 text-gray-700leading-7mt-6">
             Establishing a consistent color theme across all platforms would enhance brand recognition and user experience. While promotional ads use the signature green, black, and purple, the app and web app lean more into pastel colors with images. Since the color theme is not defined for this project, we will adhere to the current design and make necessary adjustments within this framework.
           </p>
         </div>
@@ -34,7 +34,7 @@ export const RedesignSection = () => {
               <div className="text-gray-800 w-full">
                 <div className="font-light leading-relaxed text-base text-gray-700 col-span-3">
                   <h2 className="text-xl font-semibold text-gray-800">Upgrading Navigation Bar</h2>
-                  <ul className="list-disc list-inside ml-4 text-gray-700 space-y-2 mb-6 text-gray-700 leading-8 mt-6">
+                  <ul className="list-disc list-inside ml-4 text-gray-700 space-y-2 mb-6 text-gray-700leading-7mt-6">
                     <li><span className="font-semibold">Simplify Navigation:</span> Consolidate the top hamburger menu and bottom menu into a single bottom menu called "More" to reduce user confusion and streamline access.</li>
                     <li><span className="font-semibold">Relocate In-Flight Page:</span> The In-Flight page, which informs users how to access Flair online, is not directly related to booking flights. Consider moving it to the "More" tab. Before doing so, use a heat map to analyze user interaction and determine the page's usability and frequency of use.</li>
                   </ul>
@@ -45,13 +45,15 @@ export const RedesignSection = () => {
           </div>
 
           <div className="grid grid-cols-5 gap-10 text-md h-full pt-36 pb-20">
+            <Image src={designHome} alt="issue 1" quality={95} className="rounded-xl w-full h-full object-contain bg-white col-span-3" />
+
             <section className="col-span-2 flex items-center rounded-xl">
               <div className="text-gray-800 w-full">
                 <div className="relative flex items-center mb-8">
                   <h1 className="text-2xl font-semibold text-gray-900">Enhancing the Booking Page</h1>
                 </div>
                 <div className="font-light leading-relaxed text-base text-gray-700 col-span-3">
-                  <ul className="list-disc list-inside ml-4 text-gray-700 space-y-2 leading-8 ">
+                  <ul className="list-disc list-inside ml-4 text-gray-700 space-y-2leading-7">
                     <li><span className="font-semibold">Airplane Icon Toggle:</span> The airplane icon now functions as a toggle button, simplifying the selection of start and end dates and improving usability.</li>
                     <li><span className="font-semibold">Traveler Info Display:</span> The travel box now shows detailed traveler information (e.g., "2 adults, 3 children") for clearer at-a-glance understanding.</li>
                     <li><span className="font-semibold">Optional Personalized Greeting:</span> An optional personalized greeting (e.g., "Good morning") has been added to the homepage, making the interface more welcoming.</li>
@@ -60,53 +62,68 @@ export const RedesignSection = () => {
                 </div>
               </div>
             </section>
-            <Image src={designHome} alt="issue 1" quality={95} className="rounded-xl w-full h-full object-contain bg-white col-span-3" />
           </div>
 
           <div className="grid grid-cols-5 gap-10 text-md h-full pb-36">
-            <Image src={designHome} alt="issue 2" quality={95} className="rounded-xl w-full h-full object-contain bg-white col-span-3" />
             <section className="col-span-2 flex items-center rounded-xl">
               <div className="text-gray-800 w-full">
-                <div className="relative flex items-center mb-8">
-                  <h1 className="text-2xl font-semibold text-gray-900">Additional Enhancements</h1>
-                </div>
-                <div className="font-light leading-relaxed text-base text-gray-700 col-span-3">
-                  <p>
-                    One of the key reasons Flair is among the top 5 apps in Canada is its cheap deals and affordable prices. The website provides access to these travel deals when booking flights, but the app does not.
+                <h1 className="text-2xl font-semibold text-gray-900 mb-8">Maximize Value with Flair's Budget-Friendly Deals</h1>
+                <div className="font-light leading-relaxed text-base text-gray-700  ">
+                  <p className="leading-8 ">
+                    Flair's top ranking in Canada is due to its cheap deals. The website provides these travel deals, but the app does not.
                   </p>
-                  <p><strong>Proposal:</strong></p>
-                  <ul className="list-disc list-inside ml-4 text-gray-700 space-y-2">
-                    <li><span className="font-semibold">Include Travel Deals:</span> Add a section for travel deals in the app to attract users looking for cheap rates.</li>
-                    <li><span className="font-semibold">Track User Interaction:</span> Monitor the number of clicks on the travel deals section to assess user interest.</li>
-                    <li><span className="font-semibold">Consider Menu Update:</span> If user engagement is high, create a dedicated tab in the menu for travel deals.</li>
+                  <ul className="list-disc list-inside ml-4 text-gray-700 space-y-2leading-7">
+                    <li><span className="font-semibold">Include Travel Deals:</span> Add a section for travel deals in the app to attract users.</li>
+                    <li><span className="font-semibold">Track User Interaction:</span> Monitor clicks to assess interest.</li>
+                    <li><span className="font-semibold">Consider Menu Update:</span> Create a dedicated tab for travel deals if engagement is high.</li>
                   </ul>
-                  <p>
-                    This approach ensures that customers benefit from the same deals available on the website, potentially increasing app usage and user satisfaction.
+                  <p className="leading-7 ">
+                    This ensures users get the same deals as the website, potentially increasing app usage and satisfaction.
                   </p>
                 </div>
               </div>
             </section>
+            <Image src={designHome} alt="issue 2" quality={95} className="rounded-xl w-full h-full object-contain bg-white col-span-3" />
+
           </div>
 
-          <div className="grid grid-cols-5 gap-10 text-md h-full py-30">
-            <section className="col-span-2 flex items-center rounded-xl">
+
+          <div className="grid grid-cols-11 gap-10 text-md h-full py-30">
+            <Image src={designLoggedIn} alt="issue 1" quality={95} className="rounded-xl w-full h-full object-contain bg-white col-span-7" />
+
+            <section className="col-span-4 flex items-center rounded-xl">
               <div className="text-gray-800 w-full">
                 <div className="relative flex items-center mb-8">
                   <h1 className="text-2xl font-semibold text-gray-900">Optimizing Access to Bookings</h1>
                 </div>
                 <div className="font-light leading-relaxed text-base text-gray-700 col-span-3">
-                  <ul className="list-disc list-inside ml-4 text-gray-700 space-y-2  leading-8 ">
+                  <ul className="list-disc list-inside ml-4 text-gray-700 space-y-2 leading-7">
                     <li><span className="font-semibold">Guests:</span> Flair currently lacks incentives for users to create an account. Introducing a compelling CTA button emphasizing a 'fast' experience can address this by highlighting that logging in will streamline their experience with automatic booking information display.</li>
                     <li><span className="font-semibold">Logged In Users:</span>  For logged-in users, enhance visibility of upcoming trips by adding a widget displaying their future travel plans.</li>
                   </ul>
                 </div>
               </div>
             </section>
-            <Image src={designLoggedIn} alt="issue 1" quality={95} className="rounded-xl w-full h-full object-contain bg-white col-span-3" />
+          </div>
+
+          <div className="grid grid-cols-7 gap-10 text-md h-full py-36">
+            <section className="col-span-2 flex items-center rounded-xl">
+              <div className="text-gray-800 w-full">
+                <div className="relative flex items-center mb-8">
+                  <h1 className="text-2xl font-semibold text-gray-900">Enhancing Data Retention and Accessibility for Logged-In Users</h1>
+                </div>
+                <div className="font-light leading-relaxed text-base text-gray-700 col-span-3 ">
+                  One of the most common complaints was about data retention and users not being able to find their boarding passes. To address this, we can add a notification for their boarding pass on the main page. Additionally, on the trip page, we can prominently display their boarding information and check-in time, making it easily accessible.
+                </div>
+              </div>
+            </section>
+            <Image src={designLoggedIn} alt="issue 1" quality={95} className="rounded-xl w-full h-full object-contain bg-white col-span-5" />
+
           </div>
 
           <div className="grid grid-cols-7 gap-10 text-md h-full py-36">
             <Image src={designLoggedIn} alt="issue 1" quality={95} className="rounded-xl w-full h-full object-contain bg-white col-span-5" />
+
             <section className="col-span-2 flex items-center rounded-xl">
               <div className="text-gray-800 w-full">
                 <div className="relative flex items-center mb-8">
@@ -117,20 +134,6 @@ export const RedesignSection = () => {
                 </div>
               </div>
             </section>
-          </div>
-
-          <div className="grid grid-cols-7 gap-10 text-md h-full py-36">
-            <section className="col-span-2 flex items-center rounded-xl">
-              <div className="text-gray-800 w-full">
-                <div className="relative flex items-center mb-8">
-                  <h1 className="text-2xl font-semibold text-gray-900">Enhancing Data Retention and Accessibility for Logged-In Users</h1>
-                </div>
-                <div className="font-light leading-relaxed text-base text-gray-700 col-span-3">
-                  One of the most common complaints was about data retention and users not being able to find their boarding passes. To address this, we can add a notification for their boarding pass on the main page. Additionally, on the trip page, we can prominently display their boarding information and check-in time, making it easily accessible.
-                </div>
-              </div>
-            </section>
-            <Image src={designLoggedIn} alt="issue 1" quality={95} className="rounded-xl w-full h-full object-contain bg-white col-span-5" />
           </div>
         </div>
       </div>
