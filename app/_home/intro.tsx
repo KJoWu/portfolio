7504ed40164e60642b60e0b03fc5671515474850
kim <rook@Rooks-Laptop.local>
2024-07-02 15:58:48 -0600

    prevent collapse from typewriter

--- app/_home/intro.tsx
+++ app/_home/intro.tsx
@@ -23,8 +23,8 @@ export default function Intro() {
   return (
     <div className="mb-12 pt-20 bg-gradient-to-b from-[#e5fcff] via-white to-white">
       <SectionContainer>
-        <section className={`flex flex-col md:flex-row justify-between pt-[8rem] pb-[5rem] ${inter.className}`}>
-          <div className="md:w-[24rem] relative text-center md:text-left">
+        <section className={`flex flex-col sm:flex-row justify-between pt-[8rem] pb-[5rem] ${inter.className}`}>
+          <div className="sm:w-[24rem] relative text-center sm:text-left">
             <motion.div
               initial={{ opacity: 0, scale: 0.3 }}
               animate={{ opacity: 1, scale: 1 }}
@@ -41,9 +41,8 @@ export default function Intro() {
               <span className="block h-4"></span>
               <span className={`text-6xl ${TiempoSemi.className} text-indigo-500`}>I'm Kim.</span>
 
-              <div className={`md:align-center md:items-start md:justify-left text-2xl pt-5 leading-9 flex flex-col items-center justify-center ${sourceSerif.className}`}>
-                <div className="flex items-center justify-center md:justify-left md:items-start">
-                  <span className="mr-1">I'm</span>
+              <div className={`sm:align-center sm:items-start sm:justify-left text-2xl pt-5 leading-9 flex flex-col items-center justify-center ${sourceSerif.className}`}>
+                <div className="flex justify-center sm:justify-left sm:items-start h-[3em]">
                   <Typewriter
                     options={{
                       autoStart: true,
@@ -51,12 +50,12 @@ export default function Intro() {
                     }}
 
                     onInit={(typewriter) => {
-                      typewriter.typeString('a senior frontend engineer')
-                        .pauseFor(1000)
-                        .deleteAll()
-                        .typeString('a product designer')
+                      typewriter.typeString(`I'm a senior frontend engineer`)
                         .pauseFor(2000)
-                        .deleteAll()
+                        .deleteChars(24)
+                        .typeString('product designer')
+                        .pauseFor(2000)
+                        .deleteChars(18)
                         .typeString('an artist')
                         .pauseFor(3000)
                         .deleteAll()
@@ -65,14 +64,14 @@ export default function Intro() {
                   />
                 </div>
               </div>
-              <div className={`text-center md:text-left text-lg mt-36 leading-8 ${sourceSerif.className} font-base`}>
+              <div className={`text-center sm:text-left text-lg mt-36 leading-8 ${sourceSerif.className} font-base`}>
                 <p>Recepient of the Meiya Reinking and</p>
                 <p>District of Edmonton Art Award</p>
               </div>
             </motion.div>
 
             <motion.div
-              className="flex flex-col md:flex-row mt-12 gap-6 md:gap-3"
+              className="flex flex-col sm:flex-row mt-12 gap-6 sm:gap-3"
               initial={{ opacity: 0, y: 100 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{
@@ -81,19 +80,19 @@ export default function Intro() {
             >
               <button
                 onClick={sendEmail}
-                className={`${sourceSerif.className} text-lg bg-indigo-600 md:hover:scale-110 transition hover:bg-indigo-800 rounded-lg text-white px-6 py-4 md:py-2 md:rounded-full`}
+                className={`${sourceSerif.className} text-lg bg-indigo-600 sm:hover:scale-110 transition hover:bg-indigo-800 rounded-lg text-white px-6 py-4 sm:py-2 sm:rounded-full`}
               >
                 Contact Me
               </button>
               <button
                 onClick={openNewTab}
-                className={`${sourceSerif.className} text-lg bg-white border border-1 border-indigo-600 hover:bg-indigo-100  rounded-lg px-6 py-4 md:py-2 md:rounded-full md:hover:scale-110 transition text-indigo-600`}
+                className={`${sourceSerif.className} text-lg bg-white border border-1 border-indigo-600 hover:bg-indigo-100  rounded-lg px-6 py-4 sm:py-2 sm:rounded-full sm:hover:scale-110 transition text-indigo-600`}
               >
                 My CSV
               </button>
               <button
                 onClick={openLinkedIn}
-                className="bg-white border border-1 border-indigo-600 md:hover:bg-indigo-100 px-3 md:rounded-full rounded-lg md:hover:scale-110 transition text-indigo-600 flex justify-center items-center py-4 md:py-2"
+                className="bg-white border border-1 border-indigo-600 sm:hover:bg-indigo-100 px-3 sm:rounded-full rounded-lg sm:hover:scale-110 transition text-indigo-600 flex justify-center items-center py-4 sm:py-2"
               >
                 <FaLinkedinIn className="w-5 h-5" />
               </button>
@@ -101,7 +100,7 @@ export default function Intro() {
           </div>
 
           <motion.div
-            className="hidden md:block w-[33em] h-[30em] ml-5 flex items-center justify-center"
+            className="hidden sm:block w-[33em] h-[30em] ml-5 flex items-center justify-center"
             initial={{ opacity: 0, x: 100 }}
             animate={{ opacity: 1, x: 0 }}
             transition={{
