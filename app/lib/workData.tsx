620b91e9efbe950557ee9e0c1fd0d8e1549a2d35
kim <kim.jokwah@gmail.com>
2024-10-11 01:39:51 -0600

    updated url

--- app/lib/workData.tsx
+++ app/lib/workData.tsx
@@ -35,7 +35,7 @@ export const workData: Work[] = [
     bg: "bg-zinc-50",
     tagsBg: "bg-zinc-400",
     bgHover: "hover:bg-zinc-200",
-    url: "/leetnight.com",
+    url: "https://www.leetnight.com/elev8",
     buttonColor: "#313630",
     backgroundColor: "#c1ffaa"
   },
